10181d3feb0b8806f45347c5ead9e03992daed5e
a1ienist <aoooaaooo6@gmail.com>
2024-02-08 01:03:41 +0200

    game is working but still no game over option

--- game.js
+++ game.js
@@ -2,24 +2,24 @@ const buttonColours = ["red", "blue", "green", "yellow"];
 
 const gamePattern = [];
 
-const userClickedPattern = [];
+let userClickedPattern = [];
 
 let level = 0;
 
 
 function nextSequence(max){
+
+    userClickedPattern = [];
+
+    $("h1").text("Level " + ++level);
    
     const randomNumber = Math.floor(Math.random() * max);
-
     const randomChosenColour = buttonColours[randomNumber];
     gamePattern.push(randomChosenColour);
 
     $("." + randomChosenColour).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
     playSound(randomChosenColour);
-    
-    $("h1").text("Level " + level);
-    level = level++;
-    
+
 }
 
 function playSound(name){
@@ -32,15 +32,31 @@ function animatePress(currentColour){
     $("." + currentColour).addClass("pressed");
     setTimeout(() => {
         $("." + currentColour).removeClass("pressed"), 
-        1000});
+        100});
     
-       
 }
 
-$(document).on("keydown",(event)=>{
+function checkAnswer(currentLevel){
+    if(gamePattern[currentLevel] === userClickedPattern[currentLevel]){
 
-    if(event.key === "a"){
-        nextSequence(4);
+        console.log('success');
+
+        if(gamePattern.length === userClickedPattern.length){
+            setTimeout(function () {
+                nextSequence(buttonColours.length);
+              }, 1000);
+        }
+    }else{
+        console.log('wrong');
+    }
+
+}
+
+$(document).keydown((event)=>{
+    console.log(event)
+
+    if(event.key === "a" && gamePattern.length === 0){
+        nextSequence(buttonColours.length);
     }
     
 })
@@ -48,14 +64,18 @@ $(document).on("keydown",(event)=>{
 
 
 $(".btn").on("click",(event)=>{
+
     const userChosenColour = event.target.id;
     userClickedPattern.push(userChosenColour);
-
+    
     playSound(userChosenColour);
 
     animatePress(userChosenColour);
 
     console.log(userClickedPattern);
+
+    checkAnswer(userClickedPattern.length-1);
+
 })
 
 
